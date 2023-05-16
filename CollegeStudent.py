2792aec7dd5505a209ab84e0deec323a382cd62d
Jingwen Wu <67344043+jingwenw15@users.noreply.github.com>
2023-05-15 19:41:12 -0700

    update to include shopping costs + penalty method

--- CollegeStudent.py
+++ CollegeStudent.py
@@ -2,19 +2,21 @@ import numpy as np
 
 
 class CollegeStudent: 
-    def __init__(self, budget): 
+    def __init__(self, budget=100): 
         self.budget = budget
-        self.shopping_list, self.shopping_utils = self.extract_list_and_utils()
-        print(self.shopping_utils, self.shopping_list)
+        self.shopping_list, self.shopping_utils, self.shopping_costs = self.extract_list_and_others()
+        self.shopping_utils = self.shopping_utils.reshape((self.shopping_utils.shape[0], 1))
+        self.shopping_costs = self.shopping_costs.reshape((self.shopping_costs.shape[0], 1))
 
-    def extract_list_and_utils(self): 
+    def extract_list_and_others(self): 
         file = open('shopping_list.txt', 'r')
         items_and_utils = file.read().split('\n')
-        shopping_list, shopping_utils = [], []
+        shopping_list, shopping_utils, shopping_costs = [], [], []
         for item_and_util in items_and_utils: 
-            item, util = item_and_util.split(',')
+            item, util, cost = item_and_util.split(',')
             shopping_list.append(item.strip())
             shopping_utils.append(float(util.strip()))
+            shopping_costs.append(float(cost.strip()))
 
-        return shopping_list, np.array(shopping_utils)
+        return shopping_list, np.array(shopping_utils), np.array(shopping_costs)
 
--- main.py
+++ main.py
@@ -1,4 +1,33 @@
 from CollegeStudent import CollegeStudent
+import numpy as np 
+import random 
 
 
-x = CollegeStudent(100)
+
+def penalty_method(num_its=20, penalty=500): 
+    # first assume that you want to spend the entire budget 
+    student = CollegeStudent()
+    max_util = float('-inf')
+    best_pt = None 
+    for _ in range(num_its): 
+        pt = sample_point(len(student.shopping_list))
+        total_util = np.sum(pt * student.shopping_utils) 
+        total_cost = np.sum(pt * student.shopping_costs)
+        util_plus_penalty = total_util - penalty * max(total_cost - student.budget, 0)
+        if util_plus_penalty > max_util: 
+            max_util = util_plus_penalty
+            best_pt = pt 
+    return best_pt 
+
+
+
+def sample_point(num_items, threshold=0.7): 
+    # does not use previous distribution at all 
+    pt = np.zeros((num_items, 1))
+    for i in range(num_items): 
+        keep_prob = random.random()
+        pt[i, :] = 1 if keep_prob < threshold else 0 
+    return pt 
+        
+
+penalty_method()
