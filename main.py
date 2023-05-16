83013f3bf3aac697296af0e80fad7cd8e52cb8c8
Jingwen Wu <67344043+jingwenw15@users.noreply.github.com>
2023-05-16 14:58:14 -0700

    add spending money penalty - constraint method

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from CollegeStudent import CollegeStudent
 import numpy as np 
 import random 
 
-
-def penalty_method(num_its=20, penalty=500): 
-    # first assume that you want to spend the entire budget 
+'''
+Overbudget penalty: the penalty for spending money above the budget 
+Spending penalty: the penalty for spending money in general 
+'''
+def penalty_constraint_method(num_its=20, overbudget_penalty=500, spending_penalty=0.5): 
     student = CollegeStudent()
     max_util = float('-inf')
     pt_cost = None 
@@ -13,13 +15,13 @@ def penalty_method(num_its=20, penalty=500):
         pt = sample_point(len(student.shopping_list))
         total_util = np.sum(pt * student.shopping_utils) 
         total_cost = np.sum(pt * student.shopping_costs)
-        util_plus_penalty = total_util - penalty * max(total_cost - student.budget, 0)
+        util_plus_penalty = total_util - overbudget_penalty * max(total_cost - student.budget, 0) - spending_penalty * total_cost
         if util_plus_penalty > max_util: 
             max_util = util_plus_penalty
             best_pt = pt 
             pt_cost = total_cost 
     print([student.shopping_list[i] for i, item in enumerate(best_pt) if item == 1])
-    print(f'Money spent: {pt_cost} \nUtility: {max_util}')
+    print(f'Money spent: {pt_cost} \n"Money saved: {student.budget - pt_cost}\nUtility: {max_util}')
     return best_pt 
 
 '''
@@ -34,4 +36,4 @@ def sample_point(num_items, threshold=0.7):
     return pt 
         
 
-penalty_method()
+penalty_constraint_method()
