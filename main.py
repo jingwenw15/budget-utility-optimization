37dfe886adc5ebadbc06be3d20587b623de32ab8
Jingwen Wu <67344043+jingwenw15@users.noreply.github.com>
2023-05-15 20:31:26 -0700

    fix shopping list

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import numpy as np
 import random 
 
 
-
 def penalty_method(num_its=20, penalty=500): 
     # first assume that you want to spend the entire budget 
     student = CollegeStudent()
     max_util = float('-inf')
+    pt_cost = None 
     best_pt = None 
     for _ in range(num_its): 
         pt = sample_point(len(student.shopping_list))
@@ -17,10 +17,14 @@ def penalty_method(num_its=20, penalty=500):
         if util_plus_penalty > max_util: 
             max_util = util_plus_penalty
             best_pt = pt 
+            pt_cost = total_cost 
+    print([student.shopping_list[i] for i, item in enumerate(best_pt) if item == 1])
+    print(f'Money spent: {pt_cost} \nUtility: {max_util}')
     return best_pt 
 
-
-
+'''
+For each item, we buy it with probability equal to threshold. 
+'''
 def sample_point(num_items, threshold=0.7): 
     # does not use previous distribution at all 
     pt = np.zeros((num_items, 1))
