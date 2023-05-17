ac11387a2c9914a1289a47a5c14bdabef215563b
Jingwen Wu <67344043+jingwenw15@users.noreply.github.com>
2023-05-17 10:45:47 -0700

    update to quadratic penalty

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 Overbudget penalty: the penalty for spending money above the budget 
 Spending penalty: the penalty for spending money in general 
 '''
-def penalty_constraint_method(num_its=20, overbudget_penalty=500, spending_penalty=0.5): 
+def quadratic_penalty_constraint_method(num_its=20, overbudget_penalty=500, spending_penalty=0.5): 
     student = CollegeStudent()
     max_util_with_penalty = float('-inf')
     max_util = None 
@@ -16,13 +16,13 @@ def penalty_constraint_method(num_its=20, overbudget_penalty=500, spending_penal
         pt = sample_point(len(student.shopping_list))
         total_util = np.sum(pt * student.shopping_utils) 
         total_cost = np.sum(pt * student.shopping_costs)
-        util_plus_penalty = total_util - overbudget_penalty * max(total_cost - student.budget, 0) - spending_penalty * total_cost
+        util_plus_penalty = total_util - overbudget_penalty * (max(total_cost - student.budget, 0) ** 2) - ((spending_penalty * total_cost) ** 2)
         if util_plus_penalty > max_util_with_penalty: 
             max_util_with_penalty = util_plus_penalty
             max_util = total_util 
             best_pt = pt 
             pt_cost = total_cost 
-    print([student.shopping_list[i] for i, item in enumerate(best_pt) if item == 1])
+    print('Items bought:', [student.shopping_list[i] for i, item in enumerate(best_pt) if item == 1])
     print(f'Money spent: {pt_cost} \n"Money saved: {student.budget - pt_cost}\nUtility: {max_util}')
     return best_pt 
 
@@ -38,4 +38,4 @@ def sample_point(num_items, threshold=0.7):
     return pt 
         
 
-penalty_constraint_method()
+quadratic_penalty_constraint_method()
