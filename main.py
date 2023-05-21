a53a2afc15ec3085f3ffe967851cd1481e7ac148
Jingwen Wu <67344043+jingwenw15@users.noreply.github.com>
2023-05-21 16:44:58 -0700

    move fns into separate files for organization

--- main.py
+++ main.py
@@ -1,107 +1,21 @@
-from CollegeStudent import CollegeStudent
-import numpy as np 
-import random 
 import argparse
+from binary_optimization import *
 
-
-def calc_util_and_cost(pt, shopping_utils, shopping_costs): 
-    return np.sum(pt * shopping_utils), np.sum(pt * shopping_costs)
-
-def calc_util_plus_penalty(total_util, total_cost, overbudget_penalty, spending_penalty, budget):
-    return total_util - overbudget_penalty * (max(total_cost - budget, 0) ** 2) - ((spending_penalty * total_cost) ** 2)
-
-def print_results(student, best_pt, pt_cost, max_util):
-    print('Items bought:', [student.shopping_list[i] for i, item in enumerate(best_pt) if item == 1])
-    print(f'Money spent: {pt_cost} \n"Money saved: {student.budget - pt_cost}\nUtility: {max_util}')
-
-'''
-Overbudget penalty: the penalty for spending money above the budget 
-Spending penalty: the penalty for spending money in general 
-We optimize for utility subject to the constraint of budget. 
-Another "constraint" is the spending penalty (since our second objective is saving money). 
-'''
-def quadratic_penalty_constraint_method(num_its=20, overbudget_penalty=1, spending_penalty=0.5): 
-    student = CollegeStudent()
-    max_util_with_penalty = float('-inf')
-    max_util = None 
-    pt_cost = None 
-    best_pt = None 
-    for _ in range(num_its): 
-        pt = sample_point_binary(len(student.shopping_list))
-        total_util, total_cost = calc_util_and_cost(pt, student.shopping_utils, student.shopping_costs)
-        util_plus_penalty = calc_util_plus_penalty(total_util, total_cost, overbudget_penalty, spending_penalty, student.budget)
-        if util_plus_penalty > max_util_with_penalty: 
-            max_util_with_penalty = util_plus_penalty
-            max_util = total_util 
-            best_pt = pt 
-            pt_cost = total_cost 
-    print_results(student, best_pt, pt_cost, max_util)
-    # print('Items bought:', [student.shopping_list[i] for i, item in enumerate(best_pt) if item == 1])
-    # print(f'Money spent: {pt_cost} \n"Money saved: {student.budget - pt_cost}\nUtility: {max_util}')
-    return best_pt 
-
-
-'''
-For each item, we buy it with probability equal to threshold. 
-Note that in this method, we can only buy 0 or 1 of each item. 
-'''
-def sample_point_binary(num_items, threshold=0.4): 
-    # does not use previous distribution at all 
-    pt = np.zeros((num_items, 1))
-    for i in range(num_items): 
-        keep_prob = random.random()
-        pt[i, :] = 1 if keep_prob < threshold else 0 
-    return pt 
-
-'''
-For each item, we buy it with probability equal to threshold. 
-Note that in this method, we can buy multiple of each item. 
-TODO: WIP  
-'''
-def sample_point_multi(num_items, threshold=0.4): 
-    # does not use previous distribution at all 
-    pt = np.zeros((num_items, 1))
-    for i in range(num_items): 
-        keep_prob = random.random()
-        pt[i, :] = 1 if keep_prob < threshold else 0 
-    return pt 
-        
-
-def hooke_jeeves_binary(num_its=50, overbudget_penalty=0.05, spending_penalty=0.05): 
-    student = CollegeStudent()
-    shopping_list_len = len(student.shopping_list)
-    pt = sample_point_binary(shopping_list_len)
-    best_pt = pt 
-    total_util, total_cost = calc_util_and_cost(pt, student.shopping_utils, student.shopping_costs)
-    util_plus_penalty = calc_util_plus_penalty(total_util, total_cost, overbudget_penalty, spending_penalty, student.budget)
-    for _ in range(num_its): 
-        best_pt_after_it = best_pt 
-        best_util, best_cost = total_util, total_cost
-        best_util_plus_penalty = util_plus_penalty
-        for dim in range(shopping_list_len): 
-            new_pt = best_pt.copy()
-            new_pt[dim] = 1 - best_pt[dim] # flip the purchase
-            util, cost = calc_util_and_cost(new_pt, student.shopping_utils, student.shopping_costs)
-            new_util_plus_penalty = calc_util_plus_penalty(util, cost, overbudget_penalty, spending_penalty, student.budget)
-            if new_util_plus_penalty > util_plus_penalty: 
-                best_util_plus_penalty = new_util_plus_penalty
-                best_pt_after_it = new_pt
-                best_util, best_cost = util, cost 
-        # only update everything after exploring all dimensions in an iteration 
-        total_util, total_cost = best_util, best_cost
-        util_plus_penalty = best_util_plus_penalty
-        best_pt = best_pt_after_it
-    print_results(student, best_pt, total_cost, total_util)
-    
                 
-
+""" 
+TODO:
+If we make this problem being able to buy multiple of each item (constrained by max)
+it's kind of like multiobjective multidimensional optimization problem except with 
+linear constraints. 
+Could try simplex algorithm then. Or other algorithms. 
+"""
 
 def main(): 
     parser = argparse.ArgumentParser()
     parser.add_argument('method')
     args = parser.parse_args()
     if args.method == 'quad_penalty': 
-        quadratic_penalty_constraint_method()
+        quadratic_penalty_constraint_binary()
     elif args.method == 'hooke_binary': 
         hooke_jeeves_binary() 
 
---
+++ utils.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def calc_util_and_cost(pt, shopping_utils, shopping_costs): 
+    return np.sum(pt * shopping_utils), np.sum(pt * shopping_costs)
+
+def calc_util_plus_penalty(total_util, total_cost, overbudget_penalty, spending_penalty, budget):
+    return total_util - overbudget_penalty * (max(total_cost - budget, 0) ** 2) - ((spending_penalty * total_cost) ** 2)
+
+def print_results(student, best_pt, pt_cost, max_util):
+    print('Items bought:', [student.shopping_list[i] for i, item in enumerate(best_pt) if item == 1])
+    print(f'Money spent: {pt_cost} \n"Money saved: {student.budget - pt_cost}\nUtility: {max_util}')
